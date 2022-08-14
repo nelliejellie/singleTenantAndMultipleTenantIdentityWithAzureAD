2f375fa46639a8d16f46f642846fc93aed2a9ab5
Emeka Ewelike <eewelike@infinion.co>
2022-08-14 09:26:14 +0100

    first commit

---
+++ AccountTypesMultiOrg/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AccountTypesMultiOrg.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ AccountTypesSingleOrg/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AccountTypesSingleOrg.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
